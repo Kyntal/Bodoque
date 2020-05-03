db4182d9e32a5c63e10c8efde4056ff34d7fafc0
ShompiFlen <shompire02@gmail.com>
2020-05-03 02:45:49 -0400

    Indentación y otros arreglos. No es necesario usar Plantillas literales si solo se usa un valor. En el caso de la linea 17 se justifica ya que añades "MB" al final de la cadena.
    Extraido el cliente desde el mensaje y almacenado en la variable "Bodoque".

--- Comandos/Commands/Stats.js
+++ Comandos/Commands/Stats.js
@@ -1,26 +1,27 @@
 const { MessageEmbed } = require('discord.js');
 const moment = require("moment");
 
-    module.exports = {
-        name: 'stats',
-        description: "Comando para mostrar stats del bot",
-                execute(message, args) {
-                    const actividad = moment.duration(message.client.uptime).format("D [dias], H [hrs], m [mins], s [secs]");
-                    let embed = new MessageEmbed()
-                        .setColor('GREEN') 
-                        .setAuthor(`Bot Bodoque`, message.client.user.avatarURL())
-                        .setDescription(`**Información del cliente**`)
-                        .addField(`Developer`, `Kyntal#6652`, true)
-                        .addField(`Version`, `v12`, true)
-                        .addField(`Libreria`, `Discord ^12.2.0 (Js)`, true)
-                        .addField(`Memoria`, `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`, true)
-                        .addField(`Uptime`, `${actividad}`, true)
-                        .addField(`Servidores`, `${message.client.guilds.cache.size}`, true)
-                        .addField(`Usuarios`, `${message.client.users.cache.size}`, true)
-                        .addField(`Canales`, `${message.client.channels.cache.size}`, true)
-                        .addField(`Conexiones a voz`, `${message.client.voice.connections.size}`, true)
-                    
-                    message.channel.send(embed);
-                }
-            
+module.exports = {
+  name: 'stats',
+  description: "Comando para mostrar stats del bot",
+  execute(message, args) {
+    const Bodoque = message.client;
+    const actividad = moment.duration(Bodoque.uptime).format("D [dias], H [hrs], m [mins], s [secs]");
+    const embed = new MessageEmbed()
+      .setColor('GREEN')
+      .setAuthor(`Bot Bodoque`, Bodoque.user.avatarURL())
+      .setDescription(`**Información del cliente**`)
+      .addField(`Developer`, `Kyntal#6652`, true)
+      .addField(`Versión`, `v12`, true)
+      .addField(`Libreria`, `discord.js (v12)`, true)
+      .addField(`Memoria`, `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`, true)
+      .addField(`Uptime`, actividad, true)
+      .addField(`Servidores`, Bodoque.guilds.cache.size, true)
+      .addField(`Usuarios`, Bodoque.users.cache.size, true)
+      .addField(`Canales`, Bodoque.channels.cache.size, true)
+      .addField(`Conexiones a voz`, Bodoque.voice.connections.size, true);
+
+    message.channel.send(embed);
+  }
+
 }
