eea768e24dd399e527042ffcdfd1fca60b33376d
Kyntal <clandres1985@gmail.com>
2020-05-04 16:08:26 -0400

    Update 04/05/2020

--- Comandos/Commands/ping.js
+++ Comandos/Commands/ping.js
@@ -2,6 +2,6 @@ module.exports = {
 	name: 'ping',
 	description: 'Ping!',
 	execute(message, args) {
-		message.channel.send('Pong.');
+		message.channel.send('Pong.', {files: ["https://cdn.discordapp.com/attachments/359425464885837827/706824664340627506/JauntyBeautifulBluewhale-size_restricted.gif"]});
 	},
 };
